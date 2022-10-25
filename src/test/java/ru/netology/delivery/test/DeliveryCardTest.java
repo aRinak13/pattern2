9695f7bf80699d4056d41b73df08b16f24da1d60
aRinak <grisina047@gmail.com>
2022-10-26 00:07:58 +0300

    v3

--- src/test/java/ru/netology/delivery/test/DeliveryCardTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryCardTest.java
@@ -1,8 +1,8 @@
 package ru.netology.delivery.test;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import ru.netology.delivery.data.DataGenerator;
 
@@ -17,6 +17,17 @@ import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.Keys.DELETE;
 
 public class DeliveryCardTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @BeforeEach
     void setUp() {
         open("http://localhost:9999");
